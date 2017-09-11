f21d8938642e3563639e3e9e125f3ff76cb1c7e6
akhilrs <akhil.rs@inapp.com>
2017-09-11 15:57:19 +0530

    Heroku related changes

--- simple_todo/settings.py
+++ simple_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fgtdv_+bkc&v2$mj(hckxgywl!9-21^l)_cu0j-$3knjt)yuho'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,4 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
