3ca26d370f5e29429538c7b66d4b4df163c29d62
akhilrs <mail@akhil.rs>
2017-09-19 06:33:05 +0530

    Added test webhook method

--- todo/views.py
+++ todo/views.py
@@ -8,6 +8,10 @@ from .models import Todo
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 
 def todolist(request):
     todolist = Todo.objects.filter(flag=1)
@@ -167,11 +171,75 @@ def updatetodo(request, id=''):
 
 @csrf_exempt
 def web_hook_test(request):
-    data = {
-                "speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
-                "displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-                "data": {"facebook": "Bingo"},
-                "contextOut": [{"name":"weather", "lifespan":2, "parameters":{"city":"Rome"}}],
-                "source": "DuckDuckGo"
-                }
-    return JsonResponse(data, safe=False)
+    print('Request:')
+    print(json.dumps(request, indent=4))
+    res = processRequest(request)
+
+    res = json.dumps(res, indent=4)
+    return JsonResponse(res, safe=False)
+
+
+def processRequest(req):
+    # if req.get("result").get("action") != "yahooWeatherForecast":
+    #     return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
